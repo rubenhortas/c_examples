e463bf7f785c8e5464a6b28912b396b835c65ccc
rubenhortas <rubenhortas@gmail.com>
2026-01-19 19:31:12 +0100

    feat: Add arrays example

---
+++ arrays.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int lottoNumbers[] = { 4, 8, 15, 16, 23, 41 }; // Array with known values
+    int chosenNumbers[5]; // Array with unknown values
+
+    printf("First number: '%d'\n", lottoNumbers[0]); // Arrays are initialized to 0
+    printf("Last number: '%d'\n", lottoNumbers[5]);
+
+    lottoNumbers[5] = 42;
+    printf("Last number (fixed): '%d'\n", lottoNumbers[5]);
+
+    // Assign values to the array
+    for(int i = 0; i<5; i++) {
+        chosenNumbers[i] = i*2;
+    }
+
+    printf("Chosen numbers: ");
+
+    for(int i=0; i<5; i++) {
+        printf("%d,", chosenNumbers[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
