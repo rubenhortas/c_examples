a9cc886767191587f008de48853121cab90d5322
rubenhortas <rubenhortas@gmail.com>
2026-01-20 18:06:38 +0100

    feat: Add if statements example

---
+++ if_statements.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int max(int num1, int num2);
+
+int main() {
+    int num1 = 2;
+    int num2 = 64;
+
+    printf("Max (%d, %d): %d\n", num1, num2, max(num1,num2));
+
+    return 0;
+}
+
+
+int max(int num1, int num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
