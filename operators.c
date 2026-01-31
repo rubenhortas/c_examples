90e3062c5bec929c8186578f73b5f229b179d383
rubenhortas <rubenhortas@gmail.com>
2026-01-31 14:06:14 +0100

    feat: Add operators

---
+++ operators.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // Arithmetic operators
+    int a = 15;
+    int b = 4;
+
+    int sum        = a + b;  // Addition
+    int difference = a - b;  // Subtraction
+    int product    = a * b;  // Multiplication
+    int quotient   = a / b;  // Division (Integer)
+    int remainder  = a % b;  // Modulo
+
+    printf("Arithmetic Operators\n");
+    printf("%d + %d = %d\n", a, b, sum);
+    // return: 15 + 4 = 19
+
+    printf("%d %% %d = %d (Remainder)\n\n", a, b, remainder);
+    // return: 15 % 4 = 3 (Remainder)
+
+    // Relational operators
+    printf("Relational Operators\n");
+    printf("%d == %d is %d\n", a, b, a == b);  // Equal to
+    // return: 15 == 4 is 0
+
+    printf("%d != %d is %d\n", a, b, a != b);  // Not equal to
+    // return:15 != 4 is 1
+
+    printf("%d >  %d is %d\n", a, b, a > b);   // Greater than
+    // return: 15 >  4 is 1
+
+    printf("%d <= %d is %d\n\n", a, b, a <= b); // Less than or equal to
+    // return: 15 <= 4 is 0
+
+    // Logical operators
+    bool is_sunny = true;  // true = 1
+    bool is_warm  = false; // false = 0
+
+    printf("Logical operators\n");
+    printf("Sunny AND Warm: %d\n", is_sunny && is_warm);
+    // return: Sunny AND Warm: 0
+
+    printf("Sunny OR Warm:  %d\n", is_sunny || is_warm);
+    // return: Sunny OR Warm:  1
+
+    printf("NOT Sunny:      %d\n", !is_sunny);
+    // return: NOT Sunny:      0
+
+    return 0;
+}
