63aa5a2e059b708965b475693da0d52e925e5251
rubenhortas <rubenhortas@gmail.com>
2026-01-31 13:43:03 +0100

    doc: Add comments

--- data_types.c
+++ data_types.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int i = 40;
     double d = 3.6;
     float f = 1.3;
     char c = 'A';
+    bool is_even = true;
 
     printf("int: '%d'\n", i);
+    // return: int: '40'
+
     printf("double: '%f'\n", d);
+    // return: double: '3.600000'
+
     printf("float (formatted): '%.2f'\n", f); // Two decimal places
+    // return: float (formatted): '1.30'
+
     printf("char: '%c'\n", c);
+    // return: char: 'A'
+
+    printf("is_even: %d\n", is_even);
+    // return: is_even: 1
 
     return 0;
 }
+
