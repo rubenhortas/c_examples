a255c88fa439211e66cffeeaf386a45f173390b4
rubenhortas <rubenhortas@gmail.com>
2026-01-31 13:49:59 +0100

    style: Fix variable names

--- arrays.c
+++ arrays.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int lottoNumbers[] = { 4, 8, 15, 16, 23, 41 }; // Array with known values
-    int chosenNumbers[5]; // Array with unknown values
+    int lotto_numbers[] = { 4, 8, 15, 16, 23, 41 }; // Array with known values
+    int chosen_numbers[5]; // Array with unknown values
 
-    printf("First number: '%d'\n", lottoNumbers[0]); // Arrays are initialized to 0
+    printf("First number: '%d'\n", lotto_numbers[0]); // Arrays are initialized to 0
     // return: First number: '4'
 
-    printf("Last number: '%d'\n", lottoNumbers[5]);
+    printf("Last number: '%d'\n", lotto_numbers[5]);
     // return: Last number: '41'
 
-    lottoNumbers[5] = 42;
-    printf("Last number (fixed): '%d'\n", lottoNumbers[5]);
+    lotto_numbers[5] = 42;
+    printf("Last number (fixed): '%d'\n", lotto_numbers[5]);
     // return: Last number (fixed): '42'
 
     // Assign values to the array
     for(int i = 0; i<5; i++) {
-        chosenNumbers[i] = i*2;
+        chosen_numbers[i] = i*2;
     }
 
     printf("Chosen numbers: ");
 
     for(int i=0; i<5; i++) {
-        printf("%d,", chosenNumbers[i]);
+        printf("%d,", chosen_numbers[i]);
     }
     // return: Chosen numbers: ....
 
