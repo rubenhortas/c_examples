dbe93d1a3bf8aa322da837befaa2018e74b8afda
rubenhortas <rubenhortas@gmail.com>
2026-02-02 20:37:50 +0100

    style

--- loops/while.c
+++ loops/while.c
@@ -4,17 +4,12 @@ int main() {
     int i = 0;
 
     while(i < 5) {
-        printf("%d\n", i);
+        printf("%d,", i);
         i++;
     }
-    /**
-     * return:
-     * 0
-     * 1
-     * 2
-     * 3
-     * 4
-    */
+    // return: 0,1,2,3,4,
+
+    printf("\n");
 
     return 0;
 }
