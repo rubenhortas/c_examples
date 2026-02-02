4df943986e52c7eda05751fd280542ad052552a6
rubenhortas <rubenhortas@gmail.com>
2026-02-02 19:45:45 +0100

    feat: Add while loop

---
+++ loops/while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    while(i < 5) {
+        printf("%d\n", i);
+        i++;
+    }
+    /**
+     * return:
+     * 0
+     * 1
+     * 2
+     * 3
+     * 4
+    */
+
+    return 0;
+}
