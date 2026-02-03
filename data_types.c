cbe7798d33fd69fecc8c82c73753fab0ea9b9233
rubenhortas <rubenhortas@gmail.com>
2026-02-03 22:42:22 +0100

    feat: Add new examples

--- data_types.c
+++ data_types.c
@@ -1,37 +1,176 @@
-#include <stdio.h>
+#include <float.h>
+#include <limits.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 int main() {
-    const int five = 5; // A constant never changes its value
+    /************
+     * Constants
+     * A constant value remains immutable throughout the program execution.
+     ***********/
+    const int five = 5;
 
-    int i = 40;
-    double d = 3.6;
-    float f = 1.3;
+    /*************
+     * Data types
+     ************/
+
+    short sh = SHRT_MAX;
+    unsigned short ush = USHRT_MAX;
+    int i = INT_MAX;
+    unsigned int ui = UINT_MAX;
+    long l = LONG_MAX;
+    unsigned long ul = ULONG_MAX;
+    long long ll = LLONG_MAX;
+    unsigned long long ull = ULLONG_MAX;
+    float f = FLT_MAX;  // 4 bytes
+    double d = DBL_MAX; // 8 bytes
+    long double ld = LDBL_MAX;
     char c = 'A';
-    bool is_true = true;
-    bool is_false = false;
+    char s[] = "Hello, world!";
+    bool bt = true;
+    bool bf = false;
 
-    printf("five will always be %d\n", five);
-    // return: five will always be 5
+    /********************
+     * Format specifiers
+     *******************/
 
-    printf("int: '%d'\n", i);
-    // return: int: '40'
+    printf("my constant will always be %d\n", five);
+    // output: my constant will always be 5
 
-    printf("double: '%f'\n", d);
-    // return: double: '3.600000'
+    printf("short %%hd: '%hd'\n", sh);
+    // output: short %hd: '32767'
 
-    printf("float (formatted): '%.2f'\n", f); // Two decimal places
-    // return: float (formatted): '1.30'
+    printf("unsigned short %%hu: '%hu'\n", ush);
+    // output: unsigned short %hu: '65535'
 
-    printf("char: '%c'\n", c);
-    // return: char: 'A'
+    printf("int %%d: '%d'\n", i);
+    // output: int %d: '2147483647'
 
-    printf("is_true: %d\n", is_true);
-    // return: is_true: 1
+    printf("unsigned int %%u: '%u'\n", ui);
+    // output: unsigned int %u: '4294967295'
 
-    printf("is_false: %d\n", is_false);
-    // return: is_false: 0
+    printf("long %%ld: '%ld'\n", l);
+    // output: long %ld: '9223372036854775807'
+
+    // %i can interpret numbers in octal or hexadecimal if they start with 0 or
+    // 0x, but in printf, the result of %li and %ld is the same.
+    printf("long %%li: '%li'\n", l);
+    // output: long %li: '9223372036854775807'
+
+    printf("unsigned long %%lu: '%lu'\n", ul);
+    // output: unsigned long %lu: '18446744073709551615'
+
+    // %i can interpret numbers in octal or hexadecimal if they start with 0 or
+    // 0x, but in printf, the result of %li and %ld is the same.
+    printf("unsigned long %%li: '%li'\n", ul);
+    // output: unsigned long %li: '18446744073709551615'
+
+    printf("long long %%lld: '%lld'\n", ll);
+    // output:
+
+    // %i can interpret numbers in octal or hexadecimal if they start with 0 or
+    // 0x, but in printf, the result of %lli and %lld is the same.
+    printf("long long %%lli: '%lli'\n", ll);
+    // output:
+
+    printf("unsigned long long %%llu: '%llu'\n", ull);
+    // output: unsigned long long %llu: '18446744073709551615'
+
+    printf("float %%f: '%f'\n", f);
+    // output: float %f: '340282346638528859811704183484516925440.000000'
+
+    printf("float %%e: '%e'\n", f); // Lowercase exponential form.
+    // output: float %e: '3.402823e+38'
+
+    printf("float %%E: '%E'\n", f); // Uppercase exponential form.
+    // output: float %E: '3.402823E+38'
+
+    printf("float %%g: '%g'\n", f);
+    // output: float %g: '3.40282e+38'
+
+    printf("float %%G: '%G'\n", f);
+    // output: float %G: '3.40282E+38'
+
+    printf("double %%lf: '%lf'\n", d);
+    // output: double %lf: (A very large number...)
+
+    // %f with double works for printf, but NOT for scanf.
+    printf("double %%f: '%f'\n", d);
+    // output: double %f: (A very large number...)
+
+    printf("long double %%Lf: '%Lf'\n", ld);
+    // output: long double %Lf: (Even larger...)
+
+    printf("char %%c: '%c'\n", c);
+    // output: char %c: 'A'
+
+    printf("string %%s: '%s'\n", s);
+    // output: string %s: 'Hello, world!'
+
+    printf("bool true %%d: '%d'\n", bt);
+    // output: bool true %d: '1'
+
+    printf("bool false %%d: '%d'\n", bf);
+    // output: bool false %d: '0'
+
+    /*****************
+     * Int formatting
+     ****************/
+
+    printf("unsigned octal %%o: '%o'\n", i);
+    // output: unsigned octal %o: '17777777777'
+
+    printf("unsigned hexadecimal %%x: '%x'\n", i); // Lowercase.
+    // output: unsigned hexadecimal %x: '7fffffff'
+
+    printf("unsigned hexadecimal %%X: '%X'\n", i); // Uppercase.
+    // output: unsigned hexadecimal %X: '7FFFFFFF'
+
+    /*******************
+     * Float formatting
+     ******************/
+
+    // %[flags][width][.precision]specifier
+    // Width: Minimum number of characters to be printed.
+    // Precision: For %f, it specifies the number of digits after the decimal
+    // point.
+    printf("float %%.2f: '%.2f'\n", 3.1416);
+    // output: float %.2f: '3.14'
+
+    printf("float %%6.3f: '%6.3f'\n", 3.1416);
+    // output: float %6.3f: ' 3.142'
+
+    // The '-' flag forces left-alignment within the specified width.
+    // Width (10) reserves space, .3 rounds, and '-' pushes the text to the
+    // left.
+    printf("float %%-10.3f: '%-10.3f'\n", 3.1416);
+    // output: float %-10.3f: '3.142     '
+
+    /********************
+     * String formatting
+     *******************/
+
+    // A number after '%' specifies the minimum field width to be printed.
+    printf("'%30s'\n", s);
+    // output: '                 Hello, world!'
+
+    // '-': Left alignment
+    printf("'%-30s'\n", s);
+    // output: 'Hello, world!                 '
+
+    // '.' Precision: For strings it means maximum characters to print.
+    printf("'%30.5s'\n", s);
+    // output: '                         Hello'
+
+    printf("'%-30.5s'\n", s);
+    // output: 'Hello                         '
+
+    /**************************
+     * Address format specifier
+     **************************/
+
+    printf("memory addres of 'i' %%p: '%p'\n", &i);
+    // output: memory addres of 'i' %p: '0x7ffc2857b4d4'
 
     return 0;
 }
-
