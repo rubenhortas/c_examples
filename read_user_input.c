1a5812684ebb54432b1564bd9057880fb333b68e
rubenhortas <rubenhortas@gmail.com>
2026-02-04 20:15:56 +0100

    style

--- read_user_input.c
+++ read_user_input.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // Read an integer
+    /***************************************************************************
+     * Read an integer
+     **************************************************************************/
     /* int age;
 
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("You are %d years old\n", age); */
 
-    // Read a string
+    /***************************************************************************
+     * Read a string
+     **************************************************************************/
     char name[20];
     printf("Enter your name: ");
     fgets(name, 20, stdin);
