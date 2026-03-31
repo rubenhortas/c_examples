e09563f1f6601b1db9fb29df0aa437fd8a010e6d
rubenhortas <rubenhortas@gmail.com>
2026-03-31 21:25:50 +0200

    feat: add debug with conditional directives

---
+++ debug.c
@@ -0,0 +1,26 @@
+/**
+ * Debug with conditional directives
+ *
+ * DEBUG is a standard convention used to enable or disable debugging-related blocks of code.
+ *
+ * Compile with debug: $ gcc -DDEBUG debug.c -o debug.out
+ * Compile to release: $ gcc debug.c -o debug.out (DEBUG is omitted)
+ */
+#include <stdio.h>
+
+int main() {
+#ifdef DEBUG
+    printf("> Debugging...\n");
+#endif
+
+    printf("Doing something important...\n");
+
+    return (0);
+}
+
+// return (debug):
+// > Debugging...
+// Doing something important...
+//
+// return (release):
+// Doing something important...
