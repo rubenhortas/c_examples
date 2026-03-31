0236400138601ce202ed1910f4343b6fb1b71847
rubenhortas <rubenhortas@gmail.com>
2026-03-31 21:34:44 +0200

    feat: add testing example

---
+++ testing.c
@@ -0,0 +1,27 @@
+/**
+ * The most direct way to perform tests in C is using the assert(expression) macro.
+ * If the expression is false (0), the program prints an error to stderr and aborts execution immediately.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    assert(sum(2, 2) == 4);
+    assert(sum(-1, 1) == 0);
+
+    printf("All test PASSED!\n");
+
+    return 0;
+}
+
+// ouput (successful):
+// All test PASSED!
+//
+// output (error):
+// test: testing.c:15: main: Assertion `sum(-1, 1) == 8' failed.
+// Abortado
