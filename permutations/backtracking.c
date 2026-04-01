a92d8ac6742ebb0428b79337db3b13404c886fc0
rubenhortas <rubenhortas@gmail.com>
2026-04-02 01:10:18 +0200

    format

--- permutations/backtracking.c
+++ permutations/backtracking.c
@@ -26,13 +26,13 @@ void swap(char *x, char *y) {
  */
 void permute(char *a, int l, int r) {
     int i;
-    if (l == r)
+    if(l == r)
         printf("%s\n", a);
     else {
-        for (i = l; i <= r; i++) {
+        for(i = l; i <= r; i++) {
             swap((a + l), (a + i));
             permute(a, l + 1, r);
-            swap((a + l), (a + i)); // Backtrack
+            swap((a + l), (a + i));  // Backtrack
         }
     }
 }
