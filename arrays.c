d74860c059ccde0c3359256d508446b580fdd3e3
rubenhortas <rubenhortas@gmail.com>
2026-04-02 01:06:31 +0200

    format

--- arrays.c
+++ arrays.c
@@ -5,11 +5,11 @@ int main() {
      * 1D array
      **************************************************************************/
 
-    int lotto_numbers[] = {4, 8, 15, 16, 23, 41}; // Array with known values
-    int chosen_numbers[5];                        // Array with unknown values
+    int lotto_numbers[] = {4, 8, 15, 16, 23, 41};  // Array with known values
+    int chosen_numbers[5];                         // Array with unknown values
 
     printf("First number: '%d'\n",
-           lotto_numbers[0]); // Arrays are initialized to 0
+           lotto_numbers[0]);  // Arrays are initialized to 0
     // output: First number: '4'
 
     printf("Last number: '%d'\n", lotto_numbers[5]);
@@ -20,13 +20,13 @@ int main() {
     // output: Last number (fixed): '42'
 
     // Assign values to the array
-    for (int i = 0; i < 5; i++) {
+    for(int i = 0; i < 5; i++) {
         chosen_numbers[i] = i * 2;
     }
 
     printf("Chosen numbers: ");
 
-    for (int i = 0; i < 5; i++) {
+    for(int i = 0; i < 5; i++) {
         printf("%d,", chosen_numbers[i]);
     }
     // output: Chosen numbers: ....
@@ -42,19 +42,17 @@ int main() {
 
     printf("2D array:\n");
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
             printf("%d ", array_2d[i][j]);
         }
 
         printf("\n");
     }
-    /**
-     * output:
-     * 2D array:
-     * 11 12
-     * 21 22
-     */
+    // output:
+    // 2D array:
+    // 11 12
+    // 21 22
 
     printf("\n");
 
@@ -63,31 +61,28 @@ int main() {
      **************************************************************************/
 
     // array[x][rows][cols];
-    int array_3d[2][3][2] = {{{111, 112}, {121, 122}, {131, 132}},
-                             {{211, 212}, {221, 222}, {231, 232}}};
+    int array_3d[2][3][2] = {{{111, 112}, {121, 122}, {131, 132}}, {{211, 212}, {221, 222}, {231, 232}}};
 
     printf("3D array:\n");
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 2; k++) {
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 3; j++) {
+            for(int k = 0; k < 2; k++) {
                 printf("%d ", array_3d[i][j][k]);
             }
             printf("\n");
         }
         printf("\n");
     }
-    /**
-     * output:
-     * 3D array:
-     * 111 112
-     * 121 122
-     * 131 132
-     *
-     * 211 212
-     * 221 222
-     * 231 232
-     */
+    // output:
+    // 3D array:
+    // 111 112
+    // 121 122
+    // 131 132
+    //
+    // 211 212
+    // 221 222
+    // 231 232
 
     return 0;
 }
