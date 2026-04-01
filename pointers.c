03e7c6589e4ee8e6e7cf4b7d56e349971d66fb58
rubenhortas <rubenhortas@gmail.com>
2026-04-02 01:13:19 +0200

    format

--- pointers.c
+++ pointers.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * A pointer is a variable that stores the memory address of another variable.
  *
@@ -12,18 +9,20 @@
  *  32bit system: 4 bytes
  *  64bit system: 8 bytes
  */
+#include <stdio.h>
+#include <stdlib.h>
 
 int do_something(int v);
 
 int main() {
     int var = 10;
 
-    int *ptr = &var;   // Pointer to an integer
-    int *nptr = NULL;  // NULL pointer: Points to nothing (safe to check)
-    void *vptr = NULL; // Void pointer
-                       // Generic type, must be cast before dereferencing
-    int *wptr = NULL;  // It contains a garbage value and points to an arbitrary
-                       // memory location.
+    int *ptr = &var;    // Pointer to an integer
+    int *nptr = NULL;   // NULL pointer: Points to nothing (safe to check)
+    void *vptr = NULL;  // Void pointer
+                        // Generic type, must be cast before dereferencing
+    int *wptr = NULL;   // It contains a garbage value and points to an arbitrary
+                        // memory location.
 
     printf("var address: '%p'\n", ptr);
     // output: var address: '0x7ffc53a53954'
@@ -43,10 +42,10 @@ int main() {
 
     int *dptr = malloc(sizeof(int));
 
-    free(dptr); // Memory is released
+    free(dptr);  // Memory is released
     // dptr is now a dangling pointer
 
-    dptr = NULL; // Reset to NULL to avoid dangling access
+    dptr = NULL;  // Reset to NULL to avoid dangling access
 
     /***************************************************************************
      * Function pointers
@@ -66,7 +65,7 @@ int main() {
      **************************************************************************/
 
     int *mlp1 = &var;
-    int **mlp2 = &mlp1; // pointer to pointer
+    int **mlp2 = &mlp1;  // pointer to pointer
 
     printf("var: '%d'\n", var);
     // output: var: '10'
@@ -81,6 +80,6 @@ int main() {
 }
 
 int do_something(int v) {
-    (void)v; // Avoid "unused parameter" warning
+    (void)v;  // Avoid "unused parameter" warning
     return 0;
 }
