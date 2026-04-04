89145e4592fea42e57e7c13deb5f9620d52b4603
rubenhortas <rubenhortas@gmail.com>
2026-04-04 19:59:32 +0200

    feat: add example using comma operator

--- loops/for.c
+++ loops/for.c
@@ -4,10 +4,19 @@ int main() {
     for(int i = 0; i < 5; i++) {
         printf("%d,", i);
     }
-
     printf("\n");
+    // output: 0,1,2,3,4,
 
+    // Using comma operator
+    for(int i = 0, j = 0; i < 5; i++, j = i * 10) {
+        printf("i: %d, j: %d\n", i, j);
+    }
+    // output:
+    // i: 0, j: 0
+    // i: 1, j: 10
+    // i: 2, j: 20
+    // i: 3, j: 30
+    // i: 4, j: 40
+    //
     return 0;
 }
-
-// output: 0,1,2,3,4,
